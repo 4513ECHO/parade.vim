7e9fee570af0e6567701f036858327a2f4bf737e
4513ECHO <4513echo@gmail.com>
2023-02-19 13:00:55 +0900

    feat: Implement detectBuffer

--- denops/parade/main.ts
+++ denops/parade/main.ts
@@ -6,6 +6,24 @@ import {
 } from "https://deno.land/x/unknownutil@v2.0.0/mod.ts";
 import { setConfig } from "./config.ts";
 
+interface Context {
+  denops: Denops;
+  bufnr: number;
+  winid: number;
+  param: Record<string, string>;
+}
+
+const patterns: Record<string, (ctx: Context) => Promise<void>> = {
+  "/room/(?<domain>[^/]+)/(?<roomAlias>[^/]+)": (ctx) => {
+    console.log(ctx);
+    return Promise.resolve();
+  },
+  "/user/(?<domain>[^/]+)/(?<userName>[^/]+)": (ctx) => {
+    console.log(ctx);
+    return Promise.resolve();
+  },
+};
+
 export async function main(denops: Denops): Promise<void> {
   denops.dispatcher = {
     setConfig(newConfig: unknown): Promise<void> {
@@ -19,7 +37,14 @@ export async function main(denops: Denops): Promise<void> {
       assertString(uri);
       assertNumber(bufnr);
       assertNumber(winid);
-      console.log({ uri, bufnr, winid });
+      for (const [pattern, callback] of Object.entries(patterns)) {
+        const matches = new RegExp(`^parade:/${pattern}$`).exec(uri);
+        if (matches) {
+          callback({ denops, bufnr, winid, param: matches.groups ?? {} });
+          return Promise.resolve();
+        }
+      }
+      denops.call("parade#_internal#print_error", `Invaild URI: ${uri}`);
       return Promise.resolve();
     },
   };
